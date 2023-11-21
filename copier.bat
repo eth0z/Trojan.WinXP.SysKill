ead38cce915ac9dbdbc4ae13589717e760382c95
Ethos <146621450+eth0z@users.noreply.github.com>
2023-11-21 17:15:18 +0000

    Update copier.bat

--- copier.bat
+++ copier.bat
@@ -5,10 +5,8 @@ setlocal enabledelayedexpansion
 set "a=0"
 set "mainFile=main.bat"
 
-REM Read content of main.bat
 set /p "mainContent="<"%mainFile%"
 
-REM Write mainContent into a new batch file
 echo !mainContent!>"!a!.bat"
 set /a "a+=1"
 
